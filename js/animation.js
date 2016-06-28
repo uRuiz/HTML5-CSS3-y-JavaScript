61425d61c6d8efd906a984061cd65a3174fb4b6d
Urko Ruiz <zaszur@gmail.com>
2016-06-28 22:02:53 +0200

    Empezando con el JS

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+var image = document.getElementById('title-img-animated');
+
+image.addEventListener('mousemove', function(evt){
+	image.style.backgroundPosition = evt.pageX * -1 / 12 + "px " + evt.pageY * -1 /12 + "px";
+});
+
+image.addEventListener('mouseleave', function(evt){
+	image.style.backgroundPosition= "center center";
+})
